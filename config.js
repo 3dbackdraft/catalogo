e4f1c4837790b5c1da79b6d55f6906a1aa940327
3dbackdraft <3d.backdraft@gmail.com>
2026-07-26 21:18:36 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.BACKDRAFT_CONFIG = {
   // Debe ser la misma URL /exec configurada en el panel de productos.
-  apiUrl: "",
+  apiUrl: "https://script.google.com/macros/s/AKfycbx2eTtVnYAq63__DmFV03hIWBaeq6BxFcLsSy6Jbs3x6h6e15FFlqluCNz87vbzNyZxZg/exec",
   // Incluir código de país sin +, espacios ni guiones. Ejemplo Argentina: 5492477...
   whatsappNumber: "5492477232120"
 };
